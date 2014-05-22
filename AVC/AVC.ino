5565bb3195053bc46c4b8d75fb242e06d1b86e61
David Barnett <david@barnett.net.nz>
2014-05-22 19:29:31 +1200

    ALL THE WAY UP TO SECTION 4! :D

--- AVC/AVC.ino
+++ AVC/AVC.ino
@@ -23,6 +23,9 @@ int pin_short_a = A0;
 int pin_short_b = A1;
 int pin_long = A5;
 
+int SPEED = 108; // out of 255
+// ~127 for the real course
+// ~150 for secondary course w/ full bat 
 
 void setup()
 {
@@ -46,13 +49,14 @@ void setup()
   analogWrite(pwm_b, 0);
   
   Serial.begin(9600);
-
   boadcastRadio( "QUAD 1" );
+  delay ( 1000 );
 }
 
 double last_meanx = 0;
 bool TURN_DECTED = false;
 int TURN_SIDE = 0;
+bool TURN_PARTIAL = false;
 #define TURN_RIGHT -1
 #define TURN_LEFT 1
 
@@ -60,25 +64,22 @@ void loop()
 {
   double wheel_left = 1.0 , wheel_right = 1.0;
   qtr.read(line_sensor_values,QTR_EMITTERS_OFF);
-  
+  update_long_range_sensor();
+
   double meanx = linesensor();
-  //Print Line Position
-  
-  Serial.print(meanx);
-  Serial.println(" ");
   
   line_wheel( &wheel_left , &wheel_right , meanx );
   
-  if (linesensor_right())
+  if (linesensor_left())
   {
-    wheel_left = 1.0;
-    wheel_right = -1.0;
+    wheel_left = -1.0;
+    wheel_right = 1.0;
     boadcastRadio( "QUAD 2" );
     TURN_DECTED = true;
-    TURN_SIDE = TURN_RIGHT;
-  } else if ( linesensor_none() )
+    TURN_SIDE = TURN_LEFT;
+    TURN_PARTIAL = false;
+  } else if ( linesensor_none() && !TURN_DECTED)
   {
-    
     wheel_left = 1.0;
     wheel_right = 1.0;
     if (last_meanx > 0)
@@ -89,34 +90,103 @@ void loop()
      wheel_left *= -1; 
     }
     boadcastRadio( "NONE!" );
-  } else 
+  } else if (!TURN_DECTED)
   {
     last_meanx = meanx;
   }
+
+  if ( TURN_SIDE == TURN_RIGHT && linesensor_none())
+  {
+    TURN_DECTED = true;
+    TURN_SIDE = TURN_RIGHT;
+    TURN_PARTIAL = false;
+    boadcastRadio( "RIGHT!" );
+  }
+
+  if ( short_left() && linesensor_none() && !TURN_DECTED)
+  {
+    TURN_SIDE = TURN_LEFT;
+    wheel_left = -1.0;
+    wheel_move(-1 , -1);
+    delay(100);
+    TURN_DECTED = true;
+    TURN_PARTIAL = false;
+  }
+
+  if ( short_right() && (meanx > 1 || meanx < -1  ))
+  {
+    TURN_SIDE = TURN_LEFT;
+    wheel_left = -1.0;
+    wheel_move(-1 , -1);
+    delay(300);
+    TURN_DECTED = true;
+    TURN_PARTIAL = false;
+  }
+
+    if ( long_range_sensor() > 350 )
+  {
+    TURN_SIDE = TURN_LEFT;
+    wheel_left = -1.0;
+    TURN_DECTED = true;
+    TURN_PARTIAL = false;
+  }
+
+  if ( linesensor_right() && long_range_sensor() > 350 )
+  {
+    
+    TURN_SIDE = TURN_RIGHT;
+    //LOGIC!
+    if (long_range_sensor() > 350)
+    {
+      TURN_SIDE = TURN_LEFT;
+      wheel_left = -1.0;
+      wheel_right = 1.0;
+      boadcastRadio( "QUAD 3" );
+      SPEED = 96;
+    }
+
+  }
+
+
 
   if (TURN_DECTED)
   {
     if (TURN_SIDE == TURN_RIGHT)
     {
-      if (meanx > -1.0 && meanx < -1.0)
+      boadcastRadio( "RIGHT" );
+      if (meanx > -1 && meanx < 1 && TURN_PARTIAL)
       {
         TURN_SIDE = 0;
         TURN_DECTED = false;  
-      } else {
-            wheel_left = 1.0;
-            wheel_right = -1.0;
+      } else if (meanx > 2.5 || (linesensor_right_line() && !linesensor_left_line()) )
+      {
+      	TURN_PARTIAL = true;
       }
+      wheel_left = 1.0;
+      wheel_right = -1.0;
+    } else if ( TURN_SIDE == TURN_LEFT )
+    {
+      boadcastRadio( "LEFT" );
+      if (meanx > -1 && meanx < 1)
+      {
+        TURN_SIDE = 0;
+        TURN_DECTED = false;  
+      } else if (meanx < -2.5 || (!linesensor_right_line() && linesensor_left_line()) )
+      {
+      	TURN_PARTIAL = true;
+      }
+      wheel_left = -1.0;
+      wheel_right = 1.0;
     } else {
       TURN_SIDE = 0;
       TURN_DECTED = false;
+      TURN_PARTIAL = false;
     }
-
   }
 
   wheel_move(wheel_left , wheel_right);
-  //wheel_move(0 , 0);
-  
-  delay(100);
+
+  delay (10);
 }
 
 double offset = 3.5;
@@ -141,34 +211,54 @@ bool linesensor_none ()
 	
   for (int i = 0; i < 8; i++)
   {
-      if ( (4000 - line_sensor_values[i] ) > 1024 ) // ADJUST ME!
+      if ( (4000 - line_sensor_values[i] ) > 2048 ) // ADJUST ME!
       	return false;
   }
   return true;
-}
-
-
-bool linesensor_right ()
-{
-	
-  for (int i = 0; i < 5; i++)
-  {
-      if ( (4000 - line_sensor_values[i] ) < 2048 ) // ADJUST ME!
-      	return false;
-  }
-  return true;	
 }
 
 
 bool linesensor_left ()
 {
 	
-  for (int i = 3; i < 8; i++)
+  for (int i = 0; i < 4; i++)
   {
-      if ( (4000 - line_sensor_values[i] ) < 2048 ) // ADJUST ME!
+      if ( (4000 - line_sensor_values[i] ) < 1024 ) // ADJUST ME!
+      	return false;
+  }
+  return true;	
+}
+
+
+bool linesensor_right  ()
+{
+	
+  for (int i = 4; i < 8; i++)
+  {
+      if ( (4000 - line_sensor_values[i] ) < 1024 ) // ADJUST ME!
       	return false;
   }
   return true;
+}
+
+bool linesensor_right_line()
+{
+	  for (int i = 4; i < 7; i++)
+  {
+      if ( (4000 - line_sensor_values[i] ) > 1024 ) // ADJUST ME!
+      	return true;
+  }
+  return false;
+}
+
+bool linesensor_left_line()
+{
+	  for (int i = 0; i < 3; i++)
+  {
+      if ( (4000 - line_sensor_values[i] ) > 1024 ) // ADJUST ME!
+      	return true;
+  }
+  return false;
 }
 
 // Short Range Function
@@ -217,19 +307,19 @@ double long_range_sensor ()
 }
 
 // Short range Sensors
-
+// Boolean sensor, ~150 when true
 bool short_left ()
 {
-	return ( analogRead(pin_short_a) < 1024 );
+	return ( analogRead(pin_short_a) < 512 );
 }
 
 bool short_right ()
 {
-	return ( analogRead(pin_short_b) < 1024 );
+	return ( analogRead(pin_short_b) < 512 );
 }
 
 // Wheel Functions
-double TURN_OFF_SET = 2.0;
+double TURN_OFF_SET = 2.5;
 void line_wheel (double* left, double* right , double meanx )
 {
    if (meanx < 0)
@@ -241,101 +331,108 @@ void line_wheel (double* left, double* right , double meanx )
      //right
      *right = (meanx - TURN_OFF_SET) / -TURN_OFF_SET;
    }
-    
 }
 
 //Apply power to wheels
 
 void wheel_move (double left , double right)
 {
+	//If the value of left/right is negative
+	// flip the polarity of the motor
   if (left < 0)
   {
     digitalWrite(dir_a , LOW);
-    left *= -1.5;
+    left *= -1;
   } else {
     digitalWrite(dir_a , HIGH);
   }
   if (right < 0)
   {
   digitalWrite(dir_b , LOW);
-    right *= -1.5;
+    right *= -1;
   } else {
     digitalWrite(dir_b , HIGH);
   }
-  
-   analogWrite(pwm_a, (int)(96*left) );     // set motor voltage to max
-   analogWrite(pwm_b, (int)(96*right));
+	//Maximum is 255  
+   analogWrite(pwm_a, (int)(SPEED*left) );     // set motor voltage to max
+   analogWrite(pwm_b, (int)(SPEED*right));
 }
 
-// Networking 
-
+// Networking (PACKET MAGIC)
+// Thank you Aurther for giving us sample code to do this
 void boadcastRadio (char transmission [])
 {
- char frame[34];
+ char packet[34];
  
- int nFrame = 33;
+ int packet_last = 33;
  unsigned int checkSumTotal = 0;
  unsigned int crc = 0;
  
- // delimter
- frame[0] = 0x7e;
+ // XBee packet magic
+ packet[0] = 0x7e;
 
  //length
- frame[1] = 0x00;
- frame[2] = 0x1e;
+ packet[1] = 0x00;
+ packet[2] = 0x1e;
 
  // API ID
- frame[3] = 0x10;
- frame[4] = 0x01;
+ packet[3] = 0x10;
+ packet[4] = 0x01;
 
  // destination 64 bit address - broadcast
- frame[5] = 0x00;
- frame[6] = 0x13;
- frame[7] = 0xa2;
- frame[8] = 0x00;
- frame[9] = 0x40;
- frame[10] = 0x6a;
- frame[11] = 0x40;
- frame[12] = 0xa4;
+ packet[5] = 0x00;
+ packet[6] = 0x13;
+ packet[7] = 0xa2;
+ packet[8] = 0x00;
+ packet[9] = 0x40;
+ packet[10] = 0x6a;
+ packet[11] = 0x40;
+ packet[12] = 0xa4;
 
  // destination 16 bit address - broadcast
- frame[13] = 0xff;
- frame[14] = 0xfe;
+ packet[13] = 0xff;
+ packet[14] = 0xfe;
  
  // no. of hops
- frame[15] = 0x00;
+ packet[15] = 0x00;
 
  // option
- frame[16] = 0x01;
+ packet[16] = 0x01;
  
  // data!
  for(int i = 17; i<33; i++)
  {
-   frame[i] = transmission[i-17];
+   packet[i] = transmission[i-17];
  }
 
- // check sum
- for(int i = 3; i<nFrame; i++)
+ // Sum all the bytes in the packet
+ for(int i = 3; i<packet_last; i++)
  {
-   checkSumTotal += frame[i];
+   checkSumTotal += packet[i];
  }
 
+ //Limit the Checksum to 0-255
  checkSumTotal = checkSumTotal & 0xff;
+ //Invert
  crc = 0xff - checkSumTotal;
- frame[nFrame] = crc;
- 
- nFrame++;
-
- for(int i=0; i<nFrame; i++)
+ packet[packet_last] = crc;
+ //Update to total length
+ packet_last++;
+ //Write out packet to Serial buffer
+ for(int i=0; i<packet_last; i++)
  {
-   Serial.write(frame[i]);
+   Serial.write(packet[i]);
  }
 }
 
 
 void print_debug ()
 {
+  //Print Line Position
   
+  Serial.print(last_meanx);
+  Serial.println(" ");
+
   Serial.print ( short_range(pin_short_a) ) ;
   Serial.print ( " , " ) ;
   Serial.println ( short_range(pin_short_b) ) ;
@@ -356,4 +453,5 @@ void print_debug ()
   Serial.println ( ""  );
   
 }
+
 
